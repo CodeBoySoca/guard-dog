6b5e910cbff2c707409692d22c7b8618b7cf2ee0
Rocky Latchman <kidmongoose@gmail.com>
2023-12-31 16:03:54 -0600

    Added jquery signin form

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+$(() => {
+    $('#signin').on('click', (e) => {
+        e.preventDefault()
+       $('#signup-card form input').remove()
+       $('#signup-card h3').text('Sign in')
+       $('<input type="email" value="" />')
+       .attr('type', 'email')
+       .attr('name', 'email')
+       .attr('placeholder', 'E-mail')
+       .appendTo('#signup-card form')
+
+       $('<input type="password" value="" />')
+       .attr('type', 'password')
+       .attr('name', 'password')
+       .attr('placeholder', 'Password')
+       .appendTo('#signup-card form')
+
+       $('<input type="submit" value="Sign in" id="signin">')
+       .attr('method', 'post')
+       .appendTo('#signup-card form')
+
+    })
+})
